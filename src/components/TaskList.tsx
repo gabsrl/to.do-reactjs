40557172a1c1a337a440aa686c50f42e3cc2f02e
Gabriel Lima <grl@icomp.ufam.edu.br>
2021-12-24 12:12:15 -0400

    feat: implemented handleCreateNewTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,14 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.length === 0) return;
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 100000),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask]);    
   }
 
   function handleToggleTaskCompletion(id: number) {
